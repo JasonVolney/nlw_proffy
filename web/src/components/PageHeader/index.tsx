493a19254cba5eefe7372837488d057c52761731
Jason Volney Silva <jasonvolneysilva@gmail.com>
2020-08-11 09:03:23 -0300

    Updated server folder

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
+
 import './styles.css';
 
 interface PageHeaderProps {
@@ -25,6 +26,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
             <strong>{props.title}</strong>
          {/* { props.description ? <p>{props.description}</p>: null} */}
          { props.description && <p>{props.description}</p>}
+       
             {props.children} 
         </div>
 
--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,30 +2,53 @@ import React from 'react';
 import WhatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+
+export interface Teacher {
+    
+id: number;
+avatar: string;
+bio: string;
+cost: number;
+name: string;
+subject: string;
+whatsapp: string;
+    
+}   
+   
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
          <article className="teacher-item">
                     <header>
-                        <img src="https://avatars0.githubusercontent.com/u/54520187?s=460&u=54eda9da3b0e5e9db24bb7eb3220c5787a8009ca&v=4" alt="Jason Volney"/>
+                        <img src={teacher.avatar} alt={teacher.name}/>
                         <div>
-                            <strong>Jason Volney</strong>
-                            <span>JavaScript | TypeScript | ReactJs | React Native| NodeJs</span>
+                        <strong>{teacher.name}</strong>
+                        <span>{teacher.subject}</span>
                         </div> 
                     </header>
 
-                    <p>
-                        Entusiasta das melhores tecnologias para desenvolvimento Web / Mobile. 
-                        <br/><br/>
-                        Apaixonado por criar layouts, aplicativos e por mudar a vida das pessoas através de experiências.
-                    </p>
+                    <p>{teacher.bio}</p>
                     <footer>
                         <p>Preço/hora</p>
-                        <strong>R$80,00</strong>
-                        <button type="button">
+                        <strong>R${teacher.cost}</strong>
+                        <a 
+                        target="_blank" 
+                        onClick={createNewConnection} 
+                        href={`https://wa.me/${teacher.whatsapp}`}>
                             <img src={WhatsappIcon} alt="Whatsapp"/>
                             Entrar em contato
-                        </button>
+                        </a>
                     </footer>
                 </article>
     )
